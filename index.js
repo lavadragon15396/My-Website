ed33dd7643f842f397c170b5c5cc3e2f3a8b0e84
Lavadragon15396 <taddufort@gmail.com>
2022-08-14 18:53:49 +0200

    not .html and 404 page

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname + "/www"));
 
 app.get("/", function (req, resp) {
 	resp.sendFile(path.join(__dirname, "./www/index.html"));
@@ -13,3 +13,19 @@ app.get("/", function (req, resp) {
 app.listen(port, function () {
 	console.log(`Listening on port ${port}`);
 });
+
+app.get('/home', function(req, res) {
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+app.get('/projects', function(req, res) {
+    res.sendFile(__dirname + '/www/pages/projects.html');
+});
+
+app.get('/gallery', function(req, res) {
+    res.sendFile(__dirname + '/www/pages/gallery.html');
+});
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "./www/pages/404.html"));
+})
